185186722c6ef53814845ad4aff8f256cfe88567
emrelagoz <emreelagoz356@outlook.com>
2023-05-12 13:52:24 +0300

    variable names changes

--- main.go
+++ main.go
@@ -12,28 +12,28 @@ type Word struct {
 }
 
 func sortByA(words []string) []string {
-	wordList2 := make([]Word, len(words))
+	wordWithCountList := make([]Word, len(words))
 
 	// kelimeleri ve "a" harfi sayılarını structta tutma
 	for i, word := range words {
 		countA := strings.Count(word, "a")
-		wordList2[i] = Word{
+		wordWithCountList[i] = Word{
 			Word:  word,
 			Count: countA,
 		}
 	}
 
 	// kaç tane "a" varsa ona göre sıralama eşitse ona göre sıralama yapılan yer
-	sort.Slice(wordList2, func(i, j int) bool {
-		if wordList2[i].Count == wordList2[j].Count {
-			return len(wordList2[i].Word) > len(wordList2[j].Word)
+	sort.Slice(wordWithCountList, func(i, j int) bool {
+		if wordWithCountList[i].Count == wordWithCountList[j].Count {
+			return len(wordWithCountList[i].Word) > len(wordWithCountList[j].Word)
 		}
-		return wordList2[i].Count > wordList2[j].Count
+		return wordWithCountList[i].Count > wordWithCountList[j].Count
 	})
 
 	// sıralanmıslari gosterme yapılıyor
 	sortedWords := make([]string, len(words))
-	for i, word := range wordList2 {
+	for i, word := range wordWithCountList {
 		sortedWords[i] = word.Word
 	}
 
